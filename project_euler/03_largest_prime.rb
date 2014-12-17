f610224c9cf477962e512e51e825bde1e8da9bda
zhoux10 <5643f56c@opayq.com>
2014-12-16 23:43:36 -0800

    Finish problem 8, find greatest product within a large number

--- project_euler/03_largest_prime.rb
+++ project_euler/03_largest_prime.rb
@@ -7,13 +7,14 @@
 #   factors
 # end
 #
-def short_factors(num)
-  factors = []
-  (2...num/2).each do |divider|
-    factors << divider if num % divider == 0
-  end
-  factors
-end
+#Still not short enough...
+# def short_factors(num)
+#   factors = []
+#   (2...num/3).each do |divider|
+#     factors << divider if num % divider == 0
+#   end
+#   factors
+# end
 #
 # def prime_factors(num)
 #   factors = factors(num)
@@ -22,15 +23,22 @@ end
 #   end
 # end
 
+def prime?(num)
+  answer = true
+  (2...num).each do |divider|
+    return answer = false if num % divider == 0
+  end
+  answer
+end
+
 def prime_factors_short(num)
   i = num
   while i>0
     if num % i == 0
-      return i if short_factors(i).length < 1
+      return i if prime?(i)
     end
-    p i if i % 10000 == 0
     i-=1
   end
 end
 
-p prime_factors_short(600851475143) #THIS TAKES SO LONG TO RUN!
+p prime_factors_short(600851475143) #Finally runs!
