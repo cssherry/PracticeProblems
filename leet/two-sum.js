7812f861a81881d13255ac36934e99e859e014ed
Sherry Zhou <szhou@cloudera.com>
2025-08-13 12:40:22 -0400

    two-sum exercise

---
+++ leet/two-sum.js
@@ -0,0 +1,36 @@
+// Clarifying Questions:
+// 1. Do the numbers have to be returned in any order? Let's assume it has to be in ascending order
+// 2.
+function twoSum(nums, target) {
+    var numberToIdx = {};
+    for (var idx = 0; idx < nums.length; idx++) {
+        var currNum = nums[idx];
+        var solutionNumber = target - currNum;
+        var solutionIdx = numberToIdx[solutionNumber];
+        if (solutionIdx !== undefined) {
+            return [solutionIdx, idx];
+        }
+        else {
+            numberToIdx[currNum] = idx;
+        }
+    }
+    throw new Error("No two sum solution");
+}
+var result = twoSum([2, 7, 11, 15], 9);
+console.log(result);
+console.log(JSON.stringify(result) === '[0,1]');
+result = twoSum([3, 2, 4], 6);
+console.log(result);
+console.log(JSON.stringify(result) === '[1,2]');
+result = twoSum([3, 3], 6);
+console.log(result);
+console.log(JSON.stringify(result) === '[0,1]');
+// Additional test cases
+// Try with negative numbers
+console.log(twoSum([3, 3], 6));
+console.log(result);
+console.log(JSON.stringify(result) === '[0,1]');
+// Handles 0
+result = twoSum([-1, 3, 0], 3);
+console.log(result);
+console.log(JSON.stringify(result) === '[1,2]');
---
+++ leet/two-sum.ts
@@ -0,0 +1,39 @@
+// Clarifying Questions:
+// 1. Do the numbers have to be returned in any order? Let's assume it has to be in ascending order
+// 2.
+function twoSum(nums: number[], target: number): [number, number] {
+  const numberToIdx = {};
+  for (let idx = 0; idx < nums.length; idx++) {
+    const currNum = nums[idx];
+    const solutionNumber = target - currNum;
+    const solutionIdx = numberToIdx[solutionNumber];
+    if (solutionIdx !== undefined) {
+      return [solutionIdx, idx];
+    } else {
+      numberToIdx[currNum] = idx;
+    }
+  }
+
+  throw new Error("No two sum solution");
+}
+let result = twoSum([2, 7, 11, 15], 9);
+console.log(result);
+console.log(JSON.stringify(result) === '[0,1]');
+
+result = twoSum([3, 2, 4], 6);
+console.log(result);
+console.log(JSON.stringify(result) === '[1,2]');
+
+result = twoSum([3, 3], 6);
+console.log(result);
+console.log(JSON.stringify(result) === '[0,1]');
+
+// Additional test cases
+// Try with negative numbers
+console.log(twoSum([3, 3], 6));
+console.log(result);
+console.log(JSON.stringify(result) === '[0,1]');
+// Handles 0
+result = twoSum([-1, 3, 0], 3);
+console.log(result);
+console.log(JSON.stringify(result) === '[1,2]');
